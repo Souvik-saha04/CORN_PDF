3b4ad5ea1ff8505f8c5852294249efafc9247325
Souvik Saha <souviksh2004@gmail.com>
2026-07-22 11:22:20 +0530

    requirements changed 3

--- CornPDF_backend/CornPDF_backend/settings.py
+++ CornPDF_backend/CornPDF_backend/settings.py
@@ -10,9 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+
 from pathlib import Path
 import user_accounts.firebase_admin
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 import os 
 import cloudinary
@@ -154,3 +157,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
